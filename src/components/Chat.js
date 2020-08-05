1b2d411a5133ea5a7831c259946548f95f97f097
Nishant Nehra <nishantnehra0@gmail.com>
2020-08-05 22:17:04 +0530

    chat functionality added

---
+++ src/components/Chat.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
+import '../chat.css';
+
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [], // {content: 'some message', self: true} this is how we save msgs inside this array
+      typedMessage: '',
+    };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
+  }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    this.socket.on('connect', function () {
+      console.log('CONNECTION ESTABLISHED');
+
+      //in below line we are doing this.socket.emit()
+      //but it is inside a function so reference of this has changed here that's why we have defined this.socket as some variable
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) {
+        console.log('NEW USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) {
+      // add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
+
+  render() {
+    const { typedMessage, messages } = this.state;
+
+    return (
+      <div className="chat-container">
+        <div className="chat-header">
+          Chat
+          <img
+            src="https://image.flaticon.com/icons/svg/864/864373.svg"
+            alt="close"
+            height={17}
+          />
+        </div>
+        <div className="chat-messages">
+          {messages.map((message) => (
+            <div
+              className={
+                message.self
+                  ? 'chat-bubble self-chat'
+                  : 'chat-bubble other-chat'
+              }
+            >
+              {message.content}
+            </div>
+          ))}
+        </div>
+        <div className="chat-footer">
+          <input
+            type="text"
+            value={typedMessage}
+            onChange={(e) => this.setState({ typedMessage: e.target.value })}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(Chat);
